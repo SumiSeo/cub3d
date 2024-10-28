3e937ef9c6b0341e634f228baa0eec66a3d9ee1a
Sumi Seo <sumseo@paul-f5Ar4s5.clusters.42paris.fr>
2024-10-28 13:22:33 +0100

    feat/draw-lines-with-minilibx

--- inc/structs.h
+++ inc/structs.h
@@ -6,12 +6,14 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 10:40:59 by sumseo            #+#    #+#             */
-/*   Updated: 2024/10/28 11:45:21 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/10/28 13:19:45 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_H
 # define STRUCTS_H
+
+typedef struct s_image	t_image;
 
 /**
  * @param file An array of strings containing everything inside the .cub's file.
@@ -22,66 +24,56 @@
  */
 typedef struct s_parsing
 {
-	char			**file;
-	char			**map;
-	char			*filename;
-	short int		*rows_lens;
-	short int		map_beginning;
-}					t_parsing;
+	char				**file;
+	char				**map;
+	char				*filename;
+	short int			*rows_lens;
+	short int			map_beginning;
+}						t_parsing;
 
 typedef struct s_pos
 {
-	short int		x;
-	short int		y;
-}					t_pos;
-
-typedef struct s_stack
-{
-	t_pos			left;
-	t_pos			up;
-	t_pos			right;
-	t_pos			down;
-	struct s_stack	*next;
-}					t_stack;
+	short int			x;
+	short int			y;
+}						t_pos;
 
 typedef struct s_screen
 {
-	char			*north;
-	char			*south;
-	char			*west;
-	char			*east;
-	int				floor[3];
-	int				ceiling[3];
-	int				floor_color;
-	int				ceiling_color;
+	char				*north;
+	char				*south;
+	char				*west;
+	char				*east;
+	int					floor[3];
+	int					ceiling[3];
+	int					floor_color;
+	int					ceiling_color;
+}						t_screen;
 
-}					t_screen;
-
-typedef struct t_image
+typedef struct s_image
 {
-	void			*img_ptr;
-	int				*data;
-	void			*addr;
-	int				bits_per_pixel;
-	int				line_length;
-	int				endian;
-	int				width;
-	int				height;
-}					t_image;
-
-typedef struct t_key
-{
-	int				x;
-	int				y;
-	char			str[3];
-}					t_key;
+	void				*img_ptr;
+	int					*data;
+	void				*addr;
+	int					bits_per_pixel;
+	int					line_length;
+	int					endian;
+	int					width;
+	int					height;
+}						t_image;
 
 typedef struct s_mlx
 {
-	void			*mlx_ptr;
-	void			*win;
-	t_parsing		*parsing;
-	t_image			img;
-}					t_mlx;
+	void				*mlx_ptr;
+	void				*win;
+	t_parsing			*parsing;
+	t_image				img;
+}						t_mlx;
+
+typedef struct s_key
+{
+	int					x;
+	int					y;
+	char				str[3];
+}						t_key;
 
 #endif // __STRUCTS__H
