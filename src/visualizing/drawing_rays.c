716e135a9af0b4171177b1ac3b30778961a4aa83
Sumi Seo <sumseo@paul-f4Br4s3.clusters.42paris.fr>
2024-11-01 13:40:37 +0100

    feat/create-map

--- src/visualizing/drawing_rays.c
+++ src/visualizing/drawing_rays.c
@@ -6,46 +6,15 @@
 /*   By: sumseo <sumseo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:19:05 by sumseo            #+#    #+#             */
-/*   Updated: 2024/10/31 22:13:49 by sumseo           ###   ########.fr       */
+/*   Updated: 2024/11/01 13:38:40 by sumseo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-// void	calc_dda(t_mlx *mlx, t_ray *rays)
-// {
-// 	int	mapX;
-// 	int	mapY;
-
-// 	// map x and y is current square where ray touches
-// 	(void)mlx;
-
-// }
 #define width 640
 #define height 480
 
-void	init_rays(t_ray *rays, int pos_x, int pos_y)
-{
-	(void)pos_x;
-	(void)pos_y;
-	rays->camera_int = 0;
-	rays->player.x = 2;
-	rays->player.y = 1;
-	rays->dir.x = -1;
-	rays->dir.y = 0;
-	rays->plane.x = 0;
-	rays->plane.y = 0.66;
-	rays->ray_dir.x = 0;
-	rays->ray_dir.y = 0;
-	rays->delta_dist.x = 0;
-	rays->delta_dist.y = 0;
-	rays->step_x = 0;
-	rays->step_y = 0;
-	rays->wall_dist = 0;
-	rays->hit = 0;
-	rays->side = 0;
-	// printf("pos_x %d\n", pos_x);
-}
 void	verLine(t_mlx *info, int x, int y1, int y2, int color)
 {
 	int	y;
@@ -57,98 +26,95 @@ void	verLine(t_mlx *info, int x, int y1, int y2, int color)
 		y++;
 	}
 }
+
+void	init_rays(t_ray *rays, int pos_x, int pos_y)
+{
+	rays->camera_int = 0;
+	rays->player.x = pos_x;
+	rays->player.y = pos_y;
+	rays->dir.x = -1;
+	rays->dir.y = 0;
+	rays->plane.x = 0;
+	rays->plane.y = 0.66;
+	rays->ray_dir.x = 0;
+	rays->ray_dir.y = 0;
+	rays->delta_dist.x = 0;
+	rays->delta_dist.y = 0;
+}
 void	draw_rays_2(t_mlx *mlx)
 {
+	t_ray	*rays;
 	int		x;
-	double	cameraX;
-	double	rayDirX;
-	double	rayDirY;
 	int		mapX;
 	int		mapY;
-	double	sideDistX;
-	double	sideDistY;
-	double	deltaDistX;
-	double	deltaDistY;
 	double	perpWallDist;
 	int		stepX;
 	int		stepY;
 	int		lineHeight;
-	double	dirX;
-	double	dirY;
-	double	planeX;
-	double	planeY;
 	int		drawStart;
 	int		drawEnd;
 	int		color;
-	double	posX;
-	double	posY;
 	int		hit;
 	int		side;
 
-	dirX = -1;
-	dirY = 0;
-	planeX = 0;
-	planeY = 0.66;
+	rays = malloc(sizeof(t_ray));
+	init_rays(rays, 4, 5);
 	x = 0;
-	posX = 5; // Player's starting position
-	posY = 3; // Player's starting position
 	while (x < width)
 	{
-		cameraX = 2 * x / (double)width - 1;
-		rayDirX = dirX + planeX * cameraX;
-		rayDirY = dirY + planeY * cameraX;
-		mapX = (int)posX;
-		mapY = (int)posY;
-		deltaDistX = fabs(1 / rayDirX);
-		deltaDistY = fabs(1 / rayDirY);
+		rays->camera_int = 2 * x / (double)width - 1;
+		rays->ray_dir.x = rays->dir.x + rays->plane.x * rays->camera_int;
+		rays->ray_dir.y = rays->dir.y + rays->plane.y * rays->camera_int;
+		mapX = (int)rays->player.x;
+		mapY = (int)rays->player.y;
+		rays->delta_dist.x = fabs(1 / rays->ray_dir.x);
+		rays->delta_dist.y = fabs(1 / rays->ray_dir.y);
 		hit = 0;
-		// Initialize side distances
-		if (rayDirX < 0)
+		if (rays->ray_dir.x < 0)
 		{
 			stepX = -1;
-			sideDistX = (posX - mapX) * deltaDistX;
+			rays->side_dist.x = (rays->player.x - mapX) * rays->delta_dist.x;
 		}
 		else
 		{
 			stepX = 1;
-			sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+			rays->side_dist.x = (mapX + 1.0 - rays->player.x)
+				* rays->delta_dist.x;
 		}
-		if (rayDirY < 0)
+		if (rays->ray_dir.y < 0)
 		{
 			stepY = -1;
-			sideDistY = (posY - mapY) * deltaDistY;
+			rays->side_dist.y = (rays->player.y - mapY) * rays->delta_dist.y;
 		}
 		else
 		{
 			stepY = 1;
-			sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+			rays->side_dist.y = (mapY + 1.0 - rays->player.y)
+				* rays->delta_dist.y;
 		}
-		// Raycasting loop
 		while (hit == 0)
 		{
-			if (sideDistX < sideDistY)
+			if (rays->side_dist.x < rays->side_dist.y)
 			{
-				sideDistX += deltaDistX;
+				rays->side_dist.x += rays->delta_dist.x;
 				mapX += stepX;
 				side = 0;
 			}
 			else
 			{
-				sideDistY += deltaDistY;
+				rays->side_dist.y += rays->side_dist.y;
 				mapY += stepY;
 				side = 1;
 			}
-			// Debugging output
-			// Check if the ray has hit a wall
 			if (mlx->parsing->map[mapX][mapY] != '0')
 				hit = 1;
 		}
-		// Calculate distance to the wall based on which side was hit
 		if (side == 0)
-			perpWallDist = (mapX - posX + (1 - stepX) / 2) / rayDirX;
+			perpWallDist = (mapX - rays->player.x + (1 - stepX) / 2)
+				/ rays->ray_dir.x;
 		else
-			perpWallDist = (mapY - posY + (1 - stepY) / 2) / rayDirY;
-		// Calculate height of line to draw on screen
+			perpWallDist = (mapY - rays->player.y + (1 - stepY) / 2)
+				/ rays->ray_dir.y;
 		lineHeight = (int)(height / perpWallDist);
 		drawStart = -lineHeight / 2 + height / 2;
 		if (drawStart < 0)
@@ -156,19 +122,14 @@ void	draw_rays_2(t_mlx *mlx)
 		drawEnd = lineHeight / 2 + height / 2;
 		if (drawEnd >= height)
 			drawEnd = height - 1;
-		// Determine color based on the map value
 		if (mlx->parsing->map[mapY][mapX] == '1')
-			color = 0xFF0000; // Red for walls
+			color = 0xFF0000;
 		else if (mlx->parsing->map[mapY][mapX] == '0')
-			color = 0x00FF00; // Green for free space
-		else if (mlx->parsing->map[mapY][mapX] == 'W')
-			color = 0x0000FF; // Blue for special walls
-		// else
-		// 	color = 0xFFFF00; // Yellow for default
-		// Reduce color brightness if hit from the side
+			color = 0x00FF00;
+		else
+			color = 0xFFFFFF;
 		if (side == 1)
 			color = color / 2;
-		// Draw the vertical line for the current column
 		verLine(mlx, x, drawStart, drawEnd, color);
 		x++;
 	}
